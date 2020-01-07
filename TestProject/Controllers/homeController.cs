d6dd1374767702fecd1086e863d2478832e6fc4a
abdulaleem <a4abdulaleem@gmail.com>
2020-01-07 19:43:24 +0500

    Add project files.

---
+++ TestProject/Controllers/homeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestProject.Models;
+
+namespace TestProject.Controllers
+{
+    public class homeController : Controller
+    {
+        // GET: home
+
+        TestProjectsEntities dbcontext = new TestProjectsEntities();
+
+        public ActionResult Index()
+        {
+
+            List<SelectListItem> CourseName = new SelectList(dbcontext.Courses.ToList(), "Id", "CourseName").ToList();
+            //RoomStatusType.Insert(0, new SelectListItem() { Text = "Room type", Value = "" });
+            ViewBag.CourseDrodown = CourseName;
+            var Course =  dbcontext.Courses.ToList();
+            Student StudentModel = new Student();
+            StudentViewModel model = new StudentViewModel { StudentCoures = Course , StudnetModel = StudentModel };
+            return View(model);
+        }
+
+        public JsonResult GetCourses()
+        {
+
+            List<SelectListItem> CourseName = new SelectList(dbcontext.Courses.ToList(), "Id", "CourseName").ToList();
+            //RoomStatusType.Insert(0, new SelectListItem() { Text = "Room type", Value = "" });
+            ViewBag.CourseDrodown = CourseName;
+            var Course = dbcontext.Courses.Select(m=>m.CourseName).ToList();
+            Student StudentModel = new Student();
+            //StudentViewModel model = new StudentViewModel { StudentCoures = Course, StudnetModel = StudentModel };
+            return Json(Course,JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ TestProject/Models/StudentViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestProject.Models
+{
+    public class StudentViewModel
+    {
+        public Student StudnetModel { get; set; }
+
+        public List<Courses> StudentCoures{ get; set; }
+    }
+}
